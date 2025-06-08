0a3f9b0f96c0e78926fec6f6d1ab884137895478
John Daniel Absalud <keonagjd@gmail.com>
2025-06-08 20:22:20 +0800

    light filters + hero center

---
+++ pages/script/script.js
@@ -0,0 +1,19 @@
+// Comments just in case someone snoop on my code through inspect element
+var messageOne = "If you're here to scrutinize the logic of my code, reconsider before your head starts hurting.\n\n";
+var messageTwo = "It'd be a big mistake trying to read my HTML and JavaScript. My css however... I'm proud of the way I arranged it.\n\n";
+var messageThree = "Want to take a peek? Go to Sources`.`\n\n";
+var messageFour = "Seriously though, if you just want to take some JS functions, or see how I styled my elements, the code is open-source in my github.\n\n";
+var messageFive = "find it on: https://github.com/Jideeh1\n\n";
+var messageSix = "Just don't start complaining when you `see how ass I write code. ーｗー\n\n";
+var signature = "Yours Truly, Jideeh\n\n";
+
+console.log(messageOne + messageTwo + messageThree + messageFour + messageFive + messageSix + signature);
+
+const roles = ['ADC', 'SUPP', 'TOP', 'MID', 'JUNGLE'];
+const el = document.querySelector('.role');
+let i = 0;
+
+setInterval(() => {
+  el.style.setProperty('--role-text', `"${roles[i]}"`);
+  i = (i + 1) % roles.length;
+}, 1000);
